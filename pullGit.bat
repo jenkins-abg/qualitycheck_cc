64ccb02505222018c219908e20727d18e18615d5
jenkins-abg <69100616+jenkins-abg@users.noreply.github.com>
2020-08-06 19:21:02 +0800

    Add files via upload

---
+++ pullGit.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+:: This batch file, pulls-out data from the GitHub
+:: Author: Paul Ryan A Dedumo
+TITLE GitHub Pull
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Do you want to PullGit Repo (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+	ECHO Please wait... Internet connection.
+	:: Checking Internet connection.
+	ECHO ============================
+	ECHO INTERNET CONNECTION INFO
+	ECHO ============================
+	ping	google.com -n 1 -w 1000
+	IF errorlevel 1 ( 
+		ECHO No internet connection
+	)else (
+		ECHO Connected...
+		::Targeting dir of gitLocalRepo
+		cd "C:\work\GitLocalRepo"
+		git init
+		git status
+		git pull origin master
+		ECHO Success!
+	)
+	pause
+:END
+endlocal
---
+++ pushGit.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+:: This batch file, pulls-out data from the GitHub
+:: Author: Paul Ryan A Dedumo
+TITLE GitHub Pull
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Do you want to pushGit Repo (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+	ECHO Please wait... Internet connection.
+	:: Checking Internet connection.
+	ECHO ============================
+	ECHO INTERNET CONNECTION INFO
+	ECHO ============================
+	ping	google.com -n 1 -w 1000
+	IF errorlevel 1 ( 
+		ECHO No internet connection
+	)else (
+		ECHO Connected...
+		::Targeting dir of gitLocalRepo
+		cd "C:\work\GitLocalRepo"
+		git init
+		git status
+		git add .
+		git commit -F C:\work\commitMsg.txt
+		git push -u origin master
+		ECHO Success!
+	)
+	pause
+:END
+endlocal
